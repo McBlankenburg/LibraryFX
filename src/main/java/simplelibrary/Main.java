52e2855e193d512aa338eb46a6c15f147730c161
McBlankenburg <maciej.blankenburg@gmail.com>
2020-12-09 17:06:32 +0100

    Created Project SimpleLibraryCRM using JavaFX and MVC, primaryScene of application

---
+++ src/main/java/simplelibrary/Main.java
@@ -0,0 +1,21 @@
+package simplelibrary;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+}
+
+
