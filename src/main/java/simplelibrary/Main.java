f1b6fe5f7d8b068a936c970f9cd41ddb3cd85318
McBlankenburg <maciej.blankenburg@gmail.com>
2020-12-10 00:39:01 +0100

    created Pane it is first window in application with login and password

--- src/main/java/simplelibrary/Main.java
+++ src/main/java/simplelibrary/Main.java
@@ -1,6 +1,12 @@
 package simplelibrary;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.DialogPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 
@@ -15,7 +21,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        primaryStage.setTitle("Simple Library Customer Relationship System");
+        Pane mainPne = FXMLLoader.load(getClass().getResource("/MainView.fxml"));
+        Scene scene = new Scene(mainPne);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Simple Library CRM");
         primaryStage.show();
 
     }
