1f103f45a4630c6c10350aef6e711d78b18848a8
McBlankenburg <maciej.blankenburg@gmail.com>
2020-12-09 23:45:41 +0100

    [HOTFIX] changed JDK on 13.0.2 in POM.xml and added modules of JavaFX to module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module SimpleLibraryCRM {
+
+
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.swing;
+    requires javafx.media;
+
+    exports simplelibrary;
+}
