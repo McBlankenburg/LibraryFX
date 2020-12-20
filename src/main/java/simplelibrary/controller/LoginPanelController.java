48fb9c6621e62cc937a43db3996785d1e17447f9
McBlankenburg <maciej.blankenburg@gmail.com>
2020-12-20 20:31:16 +0100

    ahhhh

--- src/main/java/simplelibrary/controller/LoginPanelController.java
+++ src/main/java/simplelibrary/controller/LoginPanelController.java
@@ -1,23 +1,18 @@
 package simplelibrary.controller;
-
-import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.*;
-
-
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-
 import javafx.stage.Stage;
 import simplelibrary.DatabaseConnection;
-
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class LoginPanelController{
+
+public class LoginPanelController {
 
     @FXML
     public Button logInButton;
@@ -29,17 +24,23 @@ public class LoginPanelController{
     public Label informationMessage;
 
 
-    public void LogInButtonOnAction(ActionEvent actionEvent) {
+    //actions methods
+
+    public void LogInButtonOnAction() throws Exception {
         informationMessage.setText("You try to login");
 
-        if(!loginField.getText().isBlank() && !passwordField.getText().isBlank()){
+        if (!loginField.getText().isBlank() && !passwordField.getText().isBlank()) {
             //validation login&&password
             informationMessage.setText("here we check your login and password");
-            validateLoginAndPassword();
+            boolean isConnected = validateLoginAndPassword();
 
-        }else if(loginField.getText().isBlank() && !passwordField.getText().isBlank()){
+            if (isConnected) {
+                createMainPanelView();
+            }
+
+        } else if (loginField.getText().isBlank() && !passwordField.getText().isBlank()) {
             informationMessage.setText("Please enter your Login");
-        }else if(!loginField.getText().isBlank() && passwordField.getText().isBlank()){
+        } else if (!loginField.getText().isBlank() && passwordField.getText().isBlank()) {
             informationMessage.setText("Please enter your Password");
         } else {
             informationMessage.setText("Please enter Login and Password");
@@ -48,13 +49,20 @@ public class LoginPanelController{
 
     }
 
+    public void signInButtonOnAction(ActionEvent actionEvent) throws Exception {
+        createRegisterPanelView();
+    }
 
-    private void validateLoginAndPassword() {
+
+
+    //validation methods
+
+    private boolean validateLoginAndPassword() {
         DatabaseConnection connectionNow = new DatabaseConnection();
         Connection connectDB = connectionNow.getConnection();
 
 
-        String verifyLogin = "SELECT count(1) FROM users WHERE login = '" + loginField.getText() + "' AND password ='" + passwordField.getText() +"'";
+        String verifyLogin = "SELECT count(1) FROM users WHERE login = '" + loginField.getText() + "' AND password ='" + passwordField.getText() + "'";
 
         try {
             Statement statement = connectDB.createStatement();
@@ -62,158 +70,49 @@ public class LoginPanelController{
             ResultSet queryResult = statement.executeQuery(verifyLogin);
 
 
-            while(queryResult.next()){
+
+            while (queryResult.next()) {
                 if (queryResult.getInt(1) == 1) {
                     informationMessage.setText("you are logged!");
-                } else {
+                    return true;
+
+                }else{
                     informationMessage.setText("Invalid login or password. Please try again!");
+                    return false;
                 }
 
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             e.getCause();
 
         }
 
+        return false;
     }
 
-    public void createAccountForm(){
-        try{
-            Parent root = FXMLLoader.load(getClass().getResource("/view/RegisterPanelView.fxml"));
+
+    //create Panel methods
+
+    private void createRegisterPanelView() throws Exception {
+            Parent root = FXMLLoader.load(getClass().getResource
+                    ("/view/RegisterPanelView.fxml"));
+
             Stage registerStage = new Stage();
-            registerStage.setTitle("Library - login panel");
-            registerStage.setScene(new Scene(root));
+            registerStage.setTitle("University Library DBMS - Register Panel");
+            registerStage.setScene(new Scene (root));
             registerStage.show();
+    }
 
+    private void createMainPanelView() throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource
+                ("/view/MainPanelView.fxml"));
 
-    public TextField loginField;
-    public PasswordField passwordField;
-    public Label informationMessage;
-
-
-    public void LogInButtonOnAction(ActionEvent actionEvent) {
-        informationMessage.setText("You try to login");
-
-        if(loginField.getText().isBlank() == false && passwordField.getText().isBlank() == false){
-            //validation login&&password
-            informationMessage.setText("here we check your login and password");
-            validateLoginAndPassword();
-
-        }else if(loginField.getText().isBlank() == true && passwordField.getText().isBlank() == false){
-            informationMessage.setText("Please enter your Login");
-        }else if(loginField.getText().isBlank() == false && passwordField.getText().isBlank() == true){
-            informationMessage.setText("Please enter your Password");
-        } else {
-            informationMessage.setText("Please enter Login and Password");
-        }
-
-
+        Stage registerStage = new Stage();
+        registerStage.setTitle("University Library DBMS - Main Panel");
+        registerStage.setScene(new Scene (root));
+        registerStage.show();
     }
 
 
-    private void validateLoginAndPassword() {
-        DatabaseConnection connectionNow = new DatabaseConnection();
-        Connection connectDB = connectionNow.getConnection();
-
-
-        String verifyLogin = "SELECT count(1) FROM users WHERE login = '" + loginField.getText() + "' AND password ='" + passwordField.getText() +"'";
-
-        try {
-            Statement statement = connectDB.createStatement();
-            statement.executeQuery("USE library");
-            ResultSet queryResult = statement.executeQuery(verifyLogin);
-
-
-            while(queryResult.next()){
-                if (queryResult.getInt(1) == 1) {
-                    informationMessage.setText("you are logged!");
-                } else {
-                    informationMessage.setText("Invalid login or password. Please try again!");
-                }
-
-
-        }catch (Exception e) {
-            e.printStackTrace();
-            e.getCause();
-        }
-    }
-
-
-        }
-
-    public void signInButtonOnAction(ActionEvent actionEvent) {
-        createAccountForm();
-
-    }
-
-    public void createAccountForm(){
-        try{
-            Parent root = FXMLLoader.load(getClass().getResource("/view/RegisterPanelView.fxml"));
-            Stage registerStage = new Stage();
-            registerStage.setTitle("Library - login panel");
-            registerStage.setScene(new Scene(root));
-            registerStage.show();
-
-
-    public TextField loginField;
-    public PasswordField passwordField;
-    public Label informationMessage;
-
-
-    public void LogInButtonOnAction(ActionEvent actionEvent) {
-        informationMessage.setText("You try to login");
-
-        if(loginField.getText().isBlank() == false && passwordField.getText().isBlank() == false){
-            //validation login&&password
-            informationMessage.setText("here we check your login and password");
-            validateLoginAndPassword();
-
-        }else if(loginField.getText().isBlank() == true && passwordField.getText().isBlank() == false){
-            informationMessage.setText("Please enter your Login");
-        }else if(loginField.getText().isBlank() == false && passwordField.getText().isBlank() == true){
-            informationMessage.setText("Please enter your Password");
-        } else {
-            informationMessage.setText("Please enter Login and Password");
-        }
-
-
-    }
-
-
-    private void validateLoginAndPassword() {
-        DatabaseConnection connectionNow = new DatabaseConnection();
-        Connection connectDB = connectionNow.getConnection();
-
-
-        String verifyLogin = "SELECT count(1) FROM users WHERE login = '" + loginField.getText() + "' AND password ='" + passwordField.getText() +"'";
-
-        try {
-            Statement statement = connectDB.createStatement();
-            statement.executeQuery("USE library");
-            ResultSet queryResult = statement.executeQuery(verifyLogin);
-
-
-            while(queryResult.next()){
-                if (queryResult.getInt(1) == 1) {
-                    informationMessage.setText("you are logged!");
-                } else {
-                    informationMessage.setText("Invalid login or password. Please try again!");
-                }
-
-
-        }catch (Exception e) {
-            e.printStackTrace();
-            e.getCause();
-        }
-    }
-
-
-        }
-
-    public void signInButtonOnAction(ActionEvent actionEvent) {
-        createAccountForm();
-
-    }
 }
-
